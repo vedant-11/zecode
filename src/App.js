2ca458436a00ab07c8e312c79801604f5c44ba27
Vedant Singh <v.vedantsingh.11@gmail.com>
2025-05-29 14:45:51 +0530

    added zecode logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Scan from "./components/Scan";
 import Landing from "./pages/Landing";
@@ -10,7 +10,13 @@ import PaymentSuccess from "./components/PaymentSuccess";
 import BarcodeScanner from "./components/BarcodeScanner";
 function App() {
   const [productInfo, setProductInfo] = useState(null);
-
+  const [showApp, setShowApp] = useState(false);
+  useEffect(() => {
+    // Initialize any necessary state or effects here
+    setTimeout(() => {
+      setShowApp(true);
+    }, 3000); // Show app after 1 second
+  }, []);
   const handleDetected = (barcode) => {
     console.log("Detected barcode:", barcode);
     // For now, just show the raw barcode
@@ -20,14 +26,22 @@ function App() {
     // fetchProductInfo(barcode);
   };
   return (
-    <div className="md:w-[80%] mx-auto min-h-screen bg-gray-50 flex items-center flex-col justify-start">
-      <Scan />
+    <>
+      {showApp ? (
+        <div className="md:w-[80%] mx-auto min-h-screen bg-gray-50 flex items-center flex-col justify-start">
+          <Scan />
 
-      {/* <UserDetailsForm/> */}
-      {/* <MobileNumber/> */}
-      {/* <Otp/> */}
-      {/* <PaymentSuccess /> */}
-    </div>
+          {/* <UserDetailsForm/> */}
+          {/* <MobileNumber/> */}
+          {/* <Otp/> */}
+          {/* <PaymentSuccess /> */}
+        </div>
+      ) : (
+        <div className="bg-[#B11F2A] mx-auto min-h-screen w-[100vw] flex items-center flex-col justify-center">
+          <img src="zecode.png" alt="Description" />
+        </div>
+      )}
+    </>
   );
 }
 
